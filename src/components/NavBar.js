6df518b70c628382c0b8a6ca7c484fc3a43cab3f
Quadiots-OfficialWebsite <quadiots@gmail.com>
2021-04-30 14:25:26 +0530

    Second Commit - Updated Design & Made Mobile Responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,15 +5,22 @@ const NavBar = () => {
   return (
     <div className="nav">
       <div className="nav__logo">
-        <img className="nav__logoImage" src="https://firebasestorage.googleapis.com/v0/b/clone-5c1a3.appspot.com/o/QuadiotsUpdatedRemovedBg.png?alt=media&token=8ba4bea3-1d95-4816-b84b-5f916f71718c" alt="QuadiotsLogo" />
+        <img
+          className="nav__logoImage"
+          src="https://firebasestorage.googleapis.com/v0/b/clone-5c1a3.appspot.com/o/QuadiotsUpdatedRemovedBg.png?alt=media&token=8ba4bea3-1d95-4816-b84b-5f916f71718c"
+          alt="QuadiotsLogo"
+        />
         <p>QUADIOTS</p>
       </div>
       <div className="nav__contents">
-        <p>HOME</p>
-        <p>ABOUT</p>
-        <p>BLOGS</p>
-        <p>CONTACT US</p>
-        <img className="nav__avatar" src="https://i.pinimg.com/originals/56/b8/43/56b843db664163b186b726545c8ea55d.jpg" alt="Avatar" />
+        <a href="/">HOME</a>
+        <a href="/">ABOUT</a>
+        <a href="/">BLOGS</a>
+        <img
+          className="nav__avatar"
+          src="https://i.pinimg.com/originals/56/b8/43/56b843db664163b186b726545c8ea55d.jpg"
+          alt="Avatar"
+        />
       </div>
     </div>
   );
